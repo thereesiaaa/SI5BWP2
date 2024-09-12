f0041e845da0938f2759e2be20c9ee6cf2a1159c
nurrachmat <>
2024-09-12 12:50:13 +0700

    Final P2
    Skeleten Index. JS

--- P2/src/index.js
+++ P2/src/index.js
@@ -6,10 +6,20 @@ app.get('/', (req, res) => {
     res.send('Hello world');
 });
 
-app.listen(port, () => {
-    console.log('Server started on port 3000');
+app.get('/about', (req, res) => {
+    res.send('Halaman Tentang Perusahaan');
 });
 
-app.get('path', (req, res) => {
-    
+app.get('/contact', (req, res) => {
+    res.send("Halaman Contact");
+});
+
+//middleware
+app.use('*', (req,res)   =>{
+    res.status(404);
+    res.send("404, Halaman tidak ditemukan");
+});
+
+app.listen(port, () => {
+    console.log('Server started on port 3000');
 });
