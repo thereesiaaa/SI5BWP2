5ff6fc8328e0fd10c017374d6aaa435072987115
Nur Rachmat <rachmat.nur91@gmail.com>
2024-10-15 15:17:15 +0700

    CRUD
    Create Read Update Delete

--- api/routes/bukus.js
+++ api/routes/bukus.js
@@ -8,8 +8,8 @@ const Buku = require("../model/buku");
 // });
 
 
-
 //format JSON
+//insert
 router.post('/', (req, res) => {
     const buku = new Buku({
         judul : req.body.judul,
@@ -17,7 +17,7 @@ router.post('/', (req, res) => {
         genre : req.body.genre
     });
 
-    console.log(buku);
+    //console.log(buku);
     buku.save().then((createdBuku)=>{
         res.status(201).json({
                 message : "Data berhasil disimpan",
@@ -27,5 +27,45 @@ router.post('/', (req, res) => {
    
 });
 
+//select
+router.get("/",(req, res)=>{
+    Buku.find()
+    .then((documents)=>{
+        res.status(201).json({
+            message : "Get Data Buku",
+            bukus : documents
+        });
+    });
+});
+
+//delete
+router.delete('/:id', (req, res) => {
+    Buku.deleteOne({_id : req.params.id})
+    .then((result)=>{
+        res.status(200).json({
+            message : "Buku berhasil dihapus ",
+            result : result 
+        });
+    });
+});
+
+router.put('/:id', (req, res) => {
+   
+     const buku = new Buku({
+        _id : req.params.id,
+        judul : req.body.judul,
+        penulis : req.body.penulis,
+        genre : req.body.genre
+    });
+
+    Buku.updateOne({_id : req.params.id}, buku)
+    .then((hasil)=>{
+        res.status(200).json({
+            message : "Update Berhasil",
+            result : hasil    
+        });
+    });
+
+});
 
 module.exports = router;
