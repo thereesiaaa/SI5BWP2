dec601ab2d3b746d4cc265898832a7297edec91e
Nur Rachmat <hermawan@mdp.ac.id>
2024-10-24 12:59:33 +0700

    User API
    Tambah User API

--- api/controller/user.js
+++ api/controller/user.js
@@ -1,5 +1,49 @@
 const User = require("../model/user");
+const bcrypt = require("bcrypt");
 
 const signUp= (req, res)=>{
 
+    bcrypt.hash(req.body.password,10)
+    .then((hash)=>{
+        const user = new User({
+            email : req.body.email,
+            password : hash
+        });
+
+        user.save()
+        .then((result)=>{
+            res.status(202).json({
+                message : "User Created",
+                // result : result
+            });    
+        })
+        .catch((err)=>{
+            res.status(501).json({
+                message : "Internal Server Error",
+                // error : err
+            });
+        });
+    });
+
+    
+    
 };
+
+const login = (req, res)=>{
+    let fetchedUser;
+
+    User.findOne({ email : req.body.email})
+    .then((user)=>{
+        if(!user){
+            return res.status(401).json({
+                message : "Auth failed, email not exists !"
+            });
+        }
+
+        fetchedUser= user;
+
+        return bcrypt.compare(req.body.password, user.password);
+    });
+}
+
+module.exports = { signUp, login}
--- api/routes/users.js
+++ api/routes/users.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
+const UserController = require("../controller/user");
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+// router.get('/', function(req, res, next) {
+//   res.send('respond with a resource');
+// });
+
+router.post('/',UserController.signUp);
+
+router.post("/login", UserController.login);
 
 module.exports = router;
