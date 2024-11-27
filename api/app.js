69e1854674220d13ae7df2a444c97ed95fe5bf83
hermawanumdp <126551695+hermawanumdp@users.noreply.github.com>
2024-11-27 13:21:18 +0700

    Update app.js

--- api/app.js
+++ api/app.js
@@ -21,7 +21,7 @@ app.use((req,res,next)=>{
   res.setHeader(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With,Content-Type, Accept");
-  res.setHeader("Access-Control-Allow-Methods","GET, POST, PATCH, DELETE, OPTIONS");  
+  res.setHeader("Access-Control-Allow-Methods","GET, PUT, POST, PATCH, DELETE, OPTIONS");  
   next();
 });
 
